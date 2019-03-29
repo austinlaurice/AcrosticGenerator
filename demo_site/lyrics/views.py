0120fc244d3f32004b178f9fae4d99e38e455870
austinlaurice <austinlaurice@yahoo.com.tw>
2019-03-29 23:56:11 +0800

    Frontend Formatting (Not checked yet)

--- demo_site/lyrics/views.py
+++ demo_site/lyrics/views.py
@@ -1,12 +1,117 @@
 from django.shortcuts import render
 from .misc import RHYME_LIST
-# Create your views here.
+import random
+from collections import defaultdict
+import re
 
 # TODO
 def gen_model_input(rhyme, first_sentence, keywords, hid_sentence, length, pattern, selected_index):
-    pass
-    #if first_sentence
+    # rhyme is already done
+    line_count = 6
+    zero_sentence = None
+    # Need to decide how many lines first
+    if (pattern == '0' or pattern == '1') and hid_sentence != None:
+        line_count = len(hid_sentence)
+    elif length != None:
+        line_count = len(length)
+    if not first_sentence:
+        zero_sentence = gen_first_sentence(keywords) # if no keyword then random generate a sentence
 
+    # Use pattern to decide the condition of each sentence
+    # length of each sentence is decided in this section
+    ch_position = []
+    if pattern == '0': #first character of each sentence
+        for ch in hid_sentence:
+            ch_position.append([(1, ch)])
+    elif pattern == '1': #last character of each sentence
+        if length:
+            for position, ch in zip(length, hid_sentence):
+                ch_position.append([(int(position), ch)])
+        else:
+            length = [random.randint(6, 16) for _ in range(len(hid_sentence))]
+            for position, ch in zip(length, hid_sentence):
+                ch_position.append([(int(position), ch)])
+    elif pattern == '2':
+        position = 1
+        if length:
+            for ch in hid_sentence:
+                ch_position.append([(position, ch)])
+                position += 1
+        else:
+            length = [x+1+random.randint(2, 10) for x in range(len(hid_sentence))]
+            for ch in hid_sentence:
+                ch_position.append([(position, ch)])
+                position += 1
+    elif pattern == '3':
+        # selected_index 0_0 1_0 1_1 need to plus one for col index
+        selected_index = selected_index.strip().split(' ')
+        selected_position = defaultdict(list)
+        for index in selected_index:
+            row, col = index.split('_')
+            selected_position[row].append(col+1)
+        if length:
+            ch_count = 0
+            for i in range(len(length)):
+                ch_row = []
+                position_now = sorted(selected_position[i]) if i in selected_position else None
+                if position_now:
+                    for position in position_now:
+                        ch_row.append((position, hid_sentence[ch_count]))
+                        ch_count += 1
+                ch_position.append(ch_row)
+        else:
+            length = [15] * 6
+            # 6 * 15
+            ch_count = 0
+            for i in range(len(length)):
+                ch_row = []
+                position_now = sorted(selected_position[i]) if i in selected_position else None
+                if position_now:
+                    for position in position_now:
+                        ch_row.append((position, hid_sentence[ch_count]))
+                        ch_count += 1
+                ch_position.append(ch_row)
+
+    # ch_position = [[row0], [row1], ....]; rhyme = 'u'; length = [11, 12, 13]; hid_sentence = '你好'
+    # generate first sentence if there's zero sentence
+    generated_lyrics = []
+    input_sentence = ''
+    sentence_now = ''
+    for row_num, length_row in enumerate(length):
+        if row_num == 0:
+            if zero_sentence:
+                condition_count = 0
+                if len(ch_position[row_num]) != 0:
+                    condition = ''
+                    for c, p in ch_position:
+                        condition = c + ' ' + p + ' '
+                        condition_count += 1
+                    condition = str(condition_count) + ' ' + condition
+                else:
+                    condition = '0 '
+                input_sentence = 'SOS ' + zero_sentence + ' EOS ' + condition + \
+                                 '|| ' + rhyme + ' || ' + str(length_row)
+                # model need to be called by here
+                sentence_now = generate_sentence(input_sentence)
+            else:
+                sentence_now = first_sentence
+        else:
+            condition_count = 0
+            if len(ch_position[row_num]) != 0:
+                condition = ''
+                for c, p in ch_position:
+                    condition = c + ' ' + p + ' '
+                    condition_count += 1
+                condition = str(condition_count) + ' ' + condition
+            else:
+                condition = '0 '
+            input_sentence = 'SOS ' + input_sentence + ' EOS ' + condition + \
+                             '|| ' + rhyme + ' || ' + str(length_row)
+            # model need to be called by here
+            sentence_now = generate_sentence(input_sentence)
+        generated_lyrics.append(sentence_now)
+        input_sentence = sentence_now
+    return generated_lyrics
 
 # main page for lyrics demo
 def lyrics(req):
@@ -17,14 +122,18 @@ def lyrics(req):
         hid_sentence = req.POST['hidden_sentence'] if req.POST['hidden_sentence'] else None
         length = req.POST['length'] if req.POST['length'] != '' else None
         pattern = req.POST['pattern']
-        if pattern == 3:
+        if pattern == '3':
             selected_index = req.POST['selected_index']
         else:
             selected_index = None
+        print (selected_index)
+        if length != None:
+            length = re.sub('[^0-9;]','', length)
+            length = length.split(';')
         # TODO
-        # Need to decide how many lines first
-        model_input = gen_model_input(rhyme, first_sentence, keywords, hid_sentence,
-                                      length, pattern, selected_index)
+
+        #model_input = gen_model_input(rhyme, first_sentence, keywords, hid_sentence,
+        #                              length, pattern, selected_index)
 
 
 
